61001c717bdc7627516f920a991cd57fc4060c42
rkskekzzz <wkdlfflxh@naver.com>
2022-06-16 18:42:41 +0900

    ✨ main process

---
+++ main.py
@@ -0,0 +1,30 @@
+from time import sleep
+from utils.fetchData import isAlreadyCheckIn
+from utils.fetchData import checkIn
+from utils.loader import Loader
+import sys
+
+
+
+if __name__ == "__main__":
+
+  # argument = sys.argv
+  user = "kilee"
+  alreadyCheckIn = False
+
+  with Loader("...checking user status", "\033[32m Done! ✅\033[0m"):
+    try:
+      alreadyCheckIn = isAlreadyCheckIn(user)
+    except KeyError:
+      print("Library Data Fetch Error!")
+      exit(1)
+
+  if alreadyCheckIn:
+    print(f"\033[33m {user} Already Check In 🚀 \033[0m")
+  else:
+    with Loader("...update user status", "\033[32m Done! ✅\033[0m"):
+      try:
+        checkIn(user)
+      except KeyError:
+        print("Library Data Fetch Error!")
+    print("\033[34m Check In Success 🚀 \033[0m")
